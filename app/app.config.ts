28ca5a8d6feb24ab382864f55f7a971b3e5b51ba
LIX <champion1chess@gmail.com>
2026-07-25 04:07:26 +0300

    fix: small

--- app/app.config.ts
+++ app/app.config.ts
@@ -12,7 +12,8 @@ export default defineAppConfig({
     }
   },
   api: {
-    // url: 'https://rooots.ru/api',
-    url: 'http://localhost:8081/api',
+    url: '159.194.202.125:8081/api',
+    // url: 'http://rooots.ru/api',
+    // url: 'http://localhost:8081/api',
   }
 })
